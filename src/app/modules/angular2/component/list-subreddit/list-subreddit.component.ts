8d7bf79bbc622c04a5dee4f7ea09a4ea0152e820
AshwiniIral <ashwiniiral16@gmail.com>
2018-07-28 15:23:57 +0100

    'track-opinion'

--- src/app/modules/angular2/component/list-subreddit/list-subreddit.component.ts
+++ src/app/modules/angular2/component/list-subreddit/list-subreddit.component.ts
@@ -11,9 +11,14 @@ import {MatSnackBar} from '@angular/material';
 export class ListSubredditComponent implements OnInit {
 
   public subReddits: any[];
+  public likedReddits: string[];
+  public dislikedReddits: string[];
 
   constructor(public snackBar: MatSnackBar,
               private fetchSubredditService: FetchSubredditService) {
+    this.subReddits = [];
+    this.likedReddits = [];
+    this.dislikedReddits = [];
   }
 
   ngOnInit() {
@@ -43,32 +48,40 @@ export class ListSubredditComponent implements OnInit {
 
   public subredditOpinion(opinion: string, indx: string) {
     const opinionDiv = document.getElementById(opinion + indx);
-    if (this.subReddits[indx].opinion === '') { // if not liked/disliked
+    if (this.subReddits[indx].opinion === '') { // selecting opinion first liked/disliked
       this.subReddits[indx].opinion = opinion;
       opinionDiv.classList.remove('mat-light');
       if (opinion === 'thumb_up') {
         this.subReddits[indx].ups = this.subReddits[indx].ups + 1;
+        this.likedReddits.push(indx);
       } else {
         this.subReddits[indx].downs = this.subReddits[indx].downs + 1;
+        this.dislikedReddits.push(indx);
       }
     } else if ((opinion === 'thumb_up' && this.subReddits[indx].opinion === 'thumb_up') ||
-      (opinion === 'thumb_down' && this.subReddits[indx].opinion === 'thumb_down')) { // if liked again or disliked again
+      (opinion === 'thumb_down' && this.subReddits[indx].opinion === 'thumb_down')) { // if liked or disliked again
       opinionDiv.classList.add('mat-light');
       this.subReddits[indx].opinion = '';
       if (opinion === 'thumb_up') {
         this.subReddits[indx].ups = this.subReddits[indx].ups - 1;
+        this.likedReddits.splice(this.likedReddits.indexOf(indx), 1);
       } else {
         this.subReddits[indx].downs = this.subReddits[indx].downs - 1;
+        this.dislikedReddits.splice(this.dislikedReddits.indexOf(indx), 1);
       }
-    } else {
+    } else { // switching opinion
       this.subReddits[indx].opinion = opinion;
       opinionDiv.classList.remove('mat-light');
       if (opinion === 'thumb_up') {
+        this.likedReddits.push(indx);
+        this.dislikedReddits.splice(this.dislikedReddits.indexOf(indx), 1);
         this.subReddits[indx].ups = this.subReddits[indx].ups + 1;
         const prevOpinionDiv = document.getElementById('thumb_down' + indx);
         prevOpinionDiv.classList.add('mat-light');
         this.subReddits[indx].downs = this.subReddits[indx].downs - 1;
       } else {
+        this.dislikedReddits.push(indx);
+        this.likedReddits.splice(this.likedReddits.indexOf(indx), 1);
         this.subReddits[indx].downs = this.subReddits[indx].downs + 1;
         const prevOpinionDiv = document.getElementById('thumb_up' + indx);
         prevOpinionDiv.classList.add('mat-light');
